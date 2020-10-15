e4439aec9494c3d667096923b7927b9ed159cfb7
sprayone <dieguimac@yahoo.com.br>
2020-10-15 15:37:31 -0300

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,97 @@
+let resumeData = {
+    "imagebaseurl":"www.sprone.tech",
+    "name": "Diego Alves",
+    "role": "Front-end and Back-end Developer",
+    "linkedinId":" diego-alves-costa-381b617b",
+    "skypeid": "Your skypeid",
+    "roleDescription": " Apaixonado por tecnologias, atualmente estudando NodeJS, ReactJS e React Native. Gosto de games e praticar esportes nas horas vagas.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/diego-alves-costa-381b617b/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/sprayone",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"Cursando o último ano da especialização em Desenvolvimento Web e aproveitando para treinar as habilidades em desenvolvimento.",
+    "address":"Brasil",
+    "website":"www.sprone.tech",
+    "education":[
+      {
+        "UniversityName":"Instituto Federal do Sul de Minas - Campus Passos",
+        "specialization":"Tecnologias para desenvolvimento Web  ",
+        "MonthOfPassing":"Dez",
+        "YearOfPassing":"2020",
+      },
+      {
+        "UniversityName":"Instituto Federal do Sul de Minas - Campus Machado",
+        "specialization":"Licenciatura em Computação",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2019",
+      },
+      {
+        "UniversityName":"Instituto Federal do Sul de Minas - Campus Machado",
+        "specialization":"Técnico em Redes de Computadores",
+        "MonthOfPassing":"Dez",
+        "YearOfPassing":"2018",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Farmácia Santa Terezinha",
+        "specialization":"Balconista / Técnico em Tecnologia da Informação",
+        "MonthOfLeaving":"Set",
+        "YearOfLeaving":"2015 / -",
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
